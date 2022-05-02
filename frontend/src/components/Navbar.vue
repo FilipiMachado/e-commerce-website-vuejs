2e08a677dca334e33d2b4bcdcb024072d35fd58f
filipi <filipisee@hotmail.com>
2022-05-02 13:50:11 -0300

    styling navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,5 +1,34 @@
 <template>
-  <div>Navbar</div>
+  <v-app-bar app color="#3853d8" dark>
+    <v-btn icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+    <v-divider vertical class="ml-1 mr-3"></v-divider>
+    <v-btn icon>
+      <v-icon>mdi-menu</v-icon>
+    </v-btn>
+    <v-toolbar-title>
+      <span class="caption">
+        Select
+      </span><br>
+        Category
+    </v-toolbar-title>
+    <v-divider vertical class="ml-5"></v-divider>
+    <div class="navbar__main-title">
+      <h4>E-Commerce</h4>
+    </div>
+    <v-spacer></v-spacer>
+    <v-divider vertical></v-divider>
+    <v-btn icon class="mx-1">
+      <v-icon>mdi-account-outline</v-icon>
+    </v-btn>
+    <v-divider vertical></v-divider>
+    <v-btn icon class="mx-1">
+      <v-badge color="#94d0ef" content="2">
+        <v-icon>mdi-cart-outline</v-icon>
+      </v-badge>
+    </v-btn>
+  </v-app-bar>
 </template>
 
 <script>
@@ -12,5 +41,19 @@ export default {
 </script>
 
 <style scoped>
-
+.navbar__main-title {
+  position: absolute; 
+  margin-left: auto; 
+  margin-right: auto; 
+  left: 0; right: 0; 
+  text-align: center;
+}
+.v-toolbar__title {
+  font-size: 1rem !important;
+}
+.v-badge__badge {
+  font-size: 10px !important;
+  height: 18px !important;
+  min-width: 18px !important;
+}
 </style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -23,3 +23,9 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.index {
+  z-index: 99;
+}
+</style>
