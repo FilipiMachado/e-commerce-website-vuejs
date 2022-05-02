cd47c7bac5d2e9db9d445beb73473449421e18d1
filipi <filipisee@hotmail.com>
2022-05-02 17:08:02 -0300

    styling dashboard

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -137,6 +137,17 @@
             </v-toolbar>
             <v-range-slider color="blue" max="40" min="30"></v-range-slider>
           </v-card>
+          <v-card flat outlined tile>
+            <v-toolbar flat>
+              <v-icon color="black" class="mr-2">mdi-chevron-down</v-icon>
+              <strong>BRAND</strong>
+              <v-spacer></v-spacer>
+              <v-icon color="grey" small>mdi-close</v-icon>
+            </v-toolbar>
+            <v-list dense class="mt-n5">
+
+            </v-list>
+          </v-card>
         </v-col>
       </v-row>
     </v-card>
@@ -180,6 +191,112 @@ export default {
       { title: "Baseball", count: "8" },
       { title: "Golf", count: "15" },
       { title: "Skateboarding", count: "22" },
+    ],
+    brands: [
+      { title: "Lifestyle", state: true },
+      { title: "Running", state: false },
+      { title: "Training & Gym", state: true },
+      { title: "Basketball", state: false },
+    ],
+    sizes: [
+      "35",
+      "36",
+      "37",
+      "38",
+      "39",
+      "40",
+      "41",
+      "42",
+      "43",
+      "44",
+      "45",
+      "46",
+    ],
+    clothes: [
+      {
+        class: "pa-0",
+        sold: "-20%",
+        image: "1.png",
+        title: "KD 8 EXT",
+        price: "$ 145.00",
+      },
+      {
+        class: "pa-0",
+        sold: "-30%",
+        image: "2.png",
+        title: "Jordan Galaxy",
+        price: "$ 599.00",
+      },
+      {
+        class: "py-0 pl-0",
+        sold: "-17%",
+        image: "3.png",
+        title: "Nike SB Trainerendor Leathe",
+        price: "$ 190.00",
+      },
+      {
+        class: "pa-0",
+        sold: "-22%",
+        image: "4.png",
+        title: "Air Jordan Spike 40 iD",
+        price: "$ 220.00",
+      },
+      {
+        class: "pa-0",
+        sold: "-18%",
+        image: "5.png",
+        title: "Nike Air Footscape Magista Flyknit",
+        price: "$ 235.00",
+      },
+      {
+        class: "py-0 pl-0",
+        sold: "-40%",
+        image: "6.png",
+        title: "Nike Air Zoom Huarache 2k4",
+        price: "$ 190.00",
+      },
+      {
+        class: "pa-0",
+        sold: "-13%",
+        image: "7.png",
+        title: "Jordan Horizon",
+        price: "$ 230.00",
+      },
+      {
+        class: "pa-0",
+        sold: "-15%",
+        image: "8.png",
+        title: "Air Jordan xx9 Low",
+        price: "$ 185.00",
+      },
+      {
+        class: "py-0 pl-0",
+        sold: "-10%",
+        image: "9.png",
+        title: "LeBron XIII Premium AS iD",
+        price: "$ 265.00",
+      },
+      {
+        class: "pa-0",
+        sold: "-19%",
+        image: "10.png",
+        title: "Air Jordan 1 Retro Hight Nouveau",
+        price: "$ 190.00",
+      },
+      {
+        class: "pa-0",
+        sold: "-16%",
+        image: "11.png",
+        title: "Nike Air Presto",
+        price: "$ 175.00",
+      },
+      {
+        class: "py-0 pl-0",
+        sold: "-10%",
+        image: "12.png",
+        title: "KD 8 Premium AS iD",
+        price: "$ 245.00",
+      },
     ],
     }
   }
