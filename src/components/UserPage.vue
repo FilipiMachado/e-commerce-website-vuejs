ec3f9cef53da7e4eef15f4a42a46408c45f8ce46
filipi <filipisee@hotmail.com>
2022-05-04 14:45:45 -0300

    structuring/styling user page

--- src/components/UserPage.vue
+++ src/components/UserPage.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>User Page</div>
+  <div class="userpage__main-content">
+    <div class="userpage__userinfo__container">
+      <div class="userpage__userinfo__left-content">
+        <div class="userpage__userinfo__left-content__img-container">
+          <img class="userpage__userinfo__left-content__img" src="@/assets/profile_example.png" alt="">
+        </div>
+        <div class="userpage__userinfo__left-content__user-container">
+          <span class="userpage__userinfo__left-content__username">Bem-vindo, Filipi Rodrigues Machado</span>
+          <div class="userpage__userinfo__left-content__user-email__container">
+            <span class="material-symbols-outlined">
+              search
+            </span>
+            <span class="userpage__userinfo__left-content__user-email">filipisee@hotmail.com</span>
+          </div>
+        </div>
+        <div class="userpage__userinfo__left-content__user-config__container">
+          <span class="material-symbols-outlined">
+            settings
+          </span>
+        </div>
+      </div>
+      <div class="userpage__userinfo__right-content">
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,5 +37,24 @@ export default {
 </script>
 
 <style scoped>
-
+.userpage__main-content {}
+.userpage__userinfo__container {
+  display: flex;
+  margin: 40px 60px;
+}
+.userpage__userinfo__left-content {
+  display: flex;
+  padding: 40px 20px;
+  border-radius: 10px;
+  background-color: #F2F3F4;
+  align-items: center;
+}
+.userpage__userinfo__left-content__img-container {
+  margin: 0px 20px;
+}
+.userpage__userinfo__left-content__img {}
+.userpage__userinfo__left-content__user-container {
+  display: flex;
+  flex-direction: column;
+}
 </style>
