0adc4b122dca050c0cd2a21c8643753226efb629
FilipiMachado <filipisee@hotmail.com>
2022-05-13 11:06:50 -0300

    structuring/styling single product page

--- src/components/SingleProduct.vue
+++ src/components/SingleProduct.vue
@@ -18,6 +18,52 @@
         <div class="singleproduct__right-content__description-wrapper">
           <span class="singleproduct__right-content__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias id itaque architecto? Deserunt, est libero culpa atque ratione, maiores ipsum voluptate facere voluptates quae repellat perferendis beatae, cum cupiditate mollitia!</span>
         </div>
+        <div class="singleproduct__product-options__wrapper">
+          <span class="singleproduct__product-options__title">Available Options</span>
+        </div>
+        <div class="singleproduct__product-options__container">
+          <div class="singleproduct__product-options__left-wrapper">
+            <span class="singleproduct__product-options__left-title">Color</span>
+            <div class="singleproduct__product-options__left-colors-wrapper">
+              <div class="singleproduct__product-options__left-colors">color</div>
+              <div class="singleproduct__product-options__left-colors">color</div>
+              <div class="singleproduct__product-options__left-colors">color</div>
+              <div class="singleproduct__product-options__left-colors">color</div>
+            </div>
+          </div>
+          <div class="singleproduct__product-options__right-wrapper">
+            <span class="singleproduct__product-options__right-title">Size</span>
+            <div class="singleproduct__product-options__right-sizes-wrapper">
+              <div class="singleproduct__product-options__right-sizes">S</div>
+              <div class="singleproduct__product-options__right-sizes">M</div>
+              <div class="singleproduct__product-options__right-sizes">L</div>
+              <div class="singleproduct__product-options__right-sizes">XL</div>
+              <div class="singleproduct__product-options__right-sizes">XXL</div>
+            </div>
+          </div>
+        </div>
+        <div class="singleproduct__quantity__container">
+          <span class="singleproduct__quantity-title">Quantity:</span>
+          <div class="singleproduct__quantity-wrapper">
+            <span class="singleproduct__quantity-minus">-</span>
+            <span class="singleproduct__quantity">1</span>
+            <span class="singleproduct__quantity-plus">+</span>
+          </div>
+          <div class="singleproduct__quantity__add-to-cart-btn">
+            <span class="singleproduct__quantity__add-to-cart">Add To Cart</span>
+          </div>
+          <div class="singleproduct__quantity__favorite-btn">
+            <span class="singleproduct__quantity__favorite">fav icon</span>
+          </div>
+        </div>
+        <div>
+          <span>Category: </span>
+          <span>Clothing</span>
+        </div>
+        <div>
+          <span>Availability</span>
+          <span>180 Products in stock</span>
+        </div>
       </div>
     </div>
   </div>
@@ -47,7 +93,7 @@ export default {
   background-size: cover;
   background-repeat: no-repeat;
   margin: 0px 10px;
-  border-radius: 3px;
+  border-radius: 4px;
 }
 .singleproduct__right-content__wrapper {
   width: 40%;
@@ -76,20 +122,69 @@ export default {
 }
 .singleproduct__right-content__price {
   font-size: 18px;
-  color: purple;
+  color: #800080;
   font-weight: bold;
   margin-right: 10px;
 }
 .singleproduct__right-content__discounted-price {
   font-size: 18px;
-  font-weight: bold; letter-spacing: 1.5px;
+  font-weight: bold;
 }
 .singleproduct__right-content__description-wrapper {
-
+  margin-bottom: 10px;
 }
 .singleproduct__right-content__description {
-  
+  letter-spacing: 1.5px;
+  font-size: 14px;
 }
+.singleproduct__product-options__wrapper {
+  margin-bottom: 10px;
+}
+.singleproduct__product-options__title {
+  font-size: 18px;
+  font-weight: 500;
+}
+.singleproduct__product-options__container {
+  display: flex;
+}
+.singleproduct__product-options__left-wrapper {
+  display: flex;
+  flex-direction: column;
+}
+.singleproduct__product-options__left-title {
+  font-weight: 500;
+}
+.singleproduct__product-options__left-colors-wrapper {
+  display: flex;
+}
+.singleproduct__product-options__left-colors {
+  background-color: #800080;
+  color: #fff;
+  padding: 4px;
+  margin-right: 5px;
+  border-radius: 4px;
+}
+.singleproduct__product-options__right-wrapper {
+  display: flex;
+  flex-direction: column;
+}
+.singleproduct__product-options__right-title {
+  font-weight: 500;
+}
+.singleproduct__product-options__right-sizes-wrapper {
+  display: flex;
+}
+.singleproduct__product-options__right-sizes {
+  background-color: #006d80;
+  color: #fff;
+  padding: 4px;
+  margin-right: 5px;
+  border-radius: 4px;
+}
+.singleproduct__product-options__container {
+  margin-bottom: 10px;
+}
+
 
 img {
   max-height: 400px;
